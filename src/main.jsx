a6cdcfb73c5029c56a6178424dcd943904d4fa8f
RiseDial <support@risedial.com>
2025-11-29 20:28:27 -0700

    Replace App component with MorphingContours4D

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import MorphingContours4D from './MorphingContours4D';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MorphingContours4D />
   </React.StrictMode>
 );
